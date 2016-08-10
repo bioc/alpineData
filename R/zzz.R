fed1d4970512db9c4e3d058df17436f50bded246
Martin Morgan <mtmorgan@fhcrc.org>
2016-08-10 22:27:08 +0000

    add alpineData
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/alpineData@3855 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/zzz.R
@@ -0,0 +1,27 @@
+.onLoad <- function(libname, pkgname) {
+  objs <- read.csv(system.file("extdata", "metadata.csv",
+                                package="alpineData"),
+                    stringsAsFactors=FALSE)$Title
+  if (!length(objs))
+    stop("no objects found")
+ 
+  ## Functions to load objects by name:
+  ns <- asNamespace(pkgname)
+  sapply(objs,
+         function(xx) {
+           func = function(metadata = FALSE) {
+             if (!isNamespaceLoaded("ExperimentHub"))
+               attachNamespace("ExperimentHub")
+             eh <- query(ExperimentHub(), "alpineData")
+             ehid <- names(query(eh, xx))
+             if (!length(ehid))
+               stop(paste0("resource ", xx,
+                           "not found in ExperimentHub"))
+             if (metadata)
+               eh[ehid]
+             else eh[[ehid]]
+           }
+           assign(xx, func, envir=ns)
+           namespaceExport(ns, xx)
+         })
+}
